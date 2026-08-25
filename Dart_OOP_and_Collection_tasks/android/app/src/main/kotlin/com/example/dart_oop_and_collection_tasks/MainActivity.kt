feaa689ba247fa218e0459b8f60a43cb8ef50384
Nupurbhoir <nupubhoir2106@gmail.com>
2026-08-25 12:01:56 +0530

    Add Dart OOP and Collection tasks

---
+++ Dart_OOP_and_Collection_tasks/android/app/src/main/kotlin/com/example/dart_oop_and_collection_tasks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dart_oop_and_collection_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Dart_OOP_and_Collection_tasks/lib/main.dart
@@ -0,0 +1,42 @@
+import 'task1_class.dart';
+import 'task2_constructor.dart';
+import 'task3_method.dart';
+import 'task4_inheritance.dart';
+import 'task5_mixin.dart';
+import 'task6_interface.dart';
+import 'task7_list.dart';
+import 'task8_map.dart';
+import 'task9_set.dart';
+import 'task10_library_system.dart';
+
+void main() {
+  print("========== TASK 1 ==========");
+  task1();
+
+  print("\n========== TASK 2 ==========");
+  task2_constructor();
+
+  print("\n========== TASK 3 ==========");
+  task3();
+
+  print("\n========== TASK 4 ==========");
+  task4();
+
+  print("\n========== TASK 5 ==========");
+  task5();
+
+  print("\n========== TASK 6 ==========");
+  task6();
+
+  print("\n========== TASK 7 ==========");
+  task7();
+
+  print("\n========== TASK 8 ==========");
+  task8();
+
+  print("\n========== TASK 9 ==========");
+  task9();
+
+  print("\n========== TASK 10 ==========");
+  task10();
+}
